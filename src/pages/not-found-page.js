43c9da7eb1bbf373e9aa1054151e7f7de9dcd4dc
Marta Sampaio <sampaio.amm@gmail.com>
2022-03-30 18:55:51 +0100

    fix(notfound): redirect to previous location

--- src/pages/not-found-page.js
+++ src/pages/not-found-page.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import image from '../assets/not-found-1.jpg';
 import routes from '../routes';
 import './not-found-page.css';
 
 export default function NotFound() {
+
+  let navigate = useNavigate();
+
   return (
-    <div className="container">
+    <div className="error-wrapper">
       <img
         src={image}
         alt="404 error illustration showing ghosts"
@@ -15,7 +18,14 @@ export default function NotFound() {
       <div className="card">
         <h1>Oops!</h1>
         <p>It looks like you've reached a url that doesn't exist.</p>
-        <Link to={routes.Listing} className="btn">Back to our products</Link>
+        <Link to={routes.Listing} className="btn">Check our products</Link>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => navigate(-1)}
+        >
+          Go back
+        </button>
       </div>
     </div>
   );
