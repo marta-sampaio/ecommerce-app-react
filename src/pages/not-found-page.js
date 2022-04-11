214f96aeda1d5e3cea8372a122017e071607a8c3
Marta Sampaio <sampaio.amm@gmail.com>
2022-04-11 16:16:22 +0100

    fix(notfound): displays 404 page without header and footer

--- src/pages/not-found-page.js
+++ src/pages/not-found-page.js
@@ -1,37 +1,42 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import image from '../assets/not-found-1.jpg';
+import { Link, useHistory, Redirect } from 'react-router-dom';
+import image from '../assets/not-found-1.png';
 import './not-found-page.css';
 
 
 export default function NotFound() {
 
-  let history = useHistory();
+  const history = useHistory();
 
-  let back = e => {
+  const back = e => {
     e.stopPropagation();
     history.goBack();
   };
 
   return (
-    <div className="error-wrapper">
-      <img
-        src={image}
-        alt="404 error illustration showing ghosts"
-        className="img-error"
-      />
-      <div className="card">
-        <h1>Oops!</h1>
-        <p>It looks like you've reached a url that doesn't exist.</p>
-        <Link to="/listing" className="btn">Check our products</Link>
-        <button
-          onClick={back}
-          type="button"
-          className="btn"
-        >
-          Go back
-        </button>
-      </div>
-    </div>
+    <>
+      <Redirect to='/404' />
+      <main className="wrapper-error">
+        <img
+          src={image}
+          alt="404 error illustration showing ghosts"
+          className="img-error"
+        />
+        <div className="card-error">
+          <h1>Oops!</h1>
+          <p>It looks like you've reached a url that doesn't exist.</p>
+          <button className="btn-error">
+            <Link to="/listing">Check our products</Link>
+          </button>
+          <button
+            onClick={back}
+            type="button"
+            className="btn-error"
+          >
+            Go back
+          </button>
+        </div>
+      </main>
+    </>
   );
 };
