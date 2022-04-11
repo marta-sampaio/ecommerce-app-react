7f9b00d5ed74c75a7ee46c0e54744d4213acc161
Marta Sampaio <sampaio.amm@gmail.com>
2022-04-11 16:17:42 +0100

    style(productcard): update CSS

--- src/components/product-card.js
+++ src/components/product-card.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './product-card.css';
 import { Link } from 'react-router-dom';
 
+
 export default function ProductCard({ id, title, image, price, rating, currency, ratingLabel }) {
+
   return (
-    <Link to={`product/${id}`} >
-      <div className="wrapper-product-card">
-        <div className="img-wrapper">
-          <img src={image} alt={title} />
-        </div>
-        <h2>{title}</h2>
-        <div className="price">{price} {currency}</div>
-        <div className="rating">{ratingLabel}{rating.rate}</div>
+    <Link to={`product/${id}`} className="wrapper-product-card">
+      <div className="wrapper-img-prod-card">
+        <img src={image} alt={title} />
       </div>
+      <h2>{title}</h2>
+      <div className="price">{price} {currency}</div>
+      <div className="rating">{ratingLabel}{rating.rate}</div>
     </Link>
   );
 };
